fba0aad3a472f61f0cf77aabfe293c7e10f8ed2d
thesadie <sgill@flowwest.com>
2017-12-20 09:21:46 -0800

    cosumnes

--- data-raw/cosumnes_river/cosumnes_river_water_temp.R
+++ data-raw/cosumnes_river/cosumnes_river_water_temp.R
@@ -51,8 +51,58 @@ cosum_wt <- cosum_water_temp %>%
   mutate(mean_max_min = (max + min)/2) %>%
   select(date, mean_water_temp_c = mean_max_min)
 
+cosum_wt %>%
+  ggplot(aes(x = date, y = mean_water_temp_c)) +
+  geom_col()
+
 cosum_wt %>% summarise(min(date), max(date))
-# GHCND:USW00023232
-# GHCND:USW00023271 probs better
-# GHCND:USC00043038 best?
-# https://www.ncdc.noaa.gov/cdo-web/results
+sac1 <- rnoaa::ncdc(datasetid = 'GSOM', stationid = 'GHCND:USW00023271', datatypeid = 'TAVG',
+                    startdate = '2001-01-01', enddate = '2010-12-31', limit = 120, token = token)
+
+sac2 <- rnoaa::ncdc(datasetid = 'GSOM', stationid = 'GHCND:USW00023271', datatypeid = 'TAVG',
+                    startdate = '2011-01-01', enddate = '2016-12-31', limit = 120, token = token)
+
+cosum_at <- sac1$data %>%
+  bind_rows(sac2$data) %>%
+  mutate(date = as_date(ymd_hms(date))) %>%
+  select(date, mean_air_temp_c = value)
+
+
+cosum <- cosum_wt %>%
+  left_join(cosum_at) %>%
+  filter(!is.na(mean_water_temp_c))
+
+cosum %>%
+  ggplot(aes(x = mean_air_temp_c, y = mean_water_temp_c)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE)
+
+cosum_temp_model <- lm(mean_water_temp_c ~ mean_air_temp_c, data = cosum)
+summary(cosum_temp_model)
+
+sac3 <- rnoaa::ncdc(datasetid = 'GSOM', stationid = 'GHCND:USW00023271', datatypeid = 'TAVG',
+                     startdate = '1980-01-01', enddate = '1989-12-31', limit = 120, token = token)
+
+sac4 <- rnoaa::ncdc(datasetid = 'GSOM', stationid = 'GHCND:USW00023271', datatypeid = 'TAVG',
+                     startdate = '1990-01-01', enddate = '1999-12-31', limit = 120, token = token)
+
+
+sac3$data %>%
+  bind_rows(sac4$data) %>%
+  mutate(date = as_date(ymd_hms(date))) %>%
+  select(date, mean_air_temp_c = value) %>%
+  ggplot(aes(x = date, y = mean_air_temp_c)) +
+  geom_col()
+
+sac <- sac3$data %>%
+  bind_rows(sac4$data) %>%
+  mutate(date = as_date(ymd_hms(date))) %>%
+  select(date, mean_air_temp_c = value)
+
+cosum_predicted_water_temp <- predict(cosum_temp_model, sac)
+
+cosumnes_water_temp_c <- tibble(
+  date = seq.Date(ymd('1980-01-01'), ymd('1999-12-01'), by = 'month'),
+  `Cosumnes River` = cosum_predicted_water_temp)
+
+write_rds(cosumnes_water_temp_c, 'data-raw/cosumnes_river/cosumnes_water_temp_c.rds')
