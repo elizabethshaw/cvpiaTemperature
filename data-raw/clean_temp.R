a0fa4c13688736b8b24908fc9b618175791a0059
sadie <sgill@flowwest.com>
2020-06-17 17:07:38 -0700

    moves this code elsewhere

--- data-raw/clean_temp.R
+++ data-raw/clean_temp.R
@@ -1,72 +1,4 @@
-library(tidyverse)
-library(readxl)
-library(lubridate)
-library(stringr)
 
-# mike wright's notes on temperature
-
-# Here 5q_date is the 6-hourly time stamps on the 5Q output; note that it's on
-# the CalLite-CV calendar i.e. starting in 2014 (except that the SJR run starts
-# in 2011, but then stops in 2011 and picks back up in 2014 so I deleted the
-# 2011 data, and the American River run ends 9 months early... I don't know
-# why). I've named each field after its DSM stream; tempoverview.csv contains
-# the original 5Q names of the records whose values are copied here. ALL UNITS
-# IN FAHRENHEIT!
-
-# read in date mapping calLite -> calsim
-cl_dates <- read_csv('data-raw/calLite_calSim_date_mapping.csv')
-
-# read in watershed ids and names for ordering output
-cvpia_watershed_ids <- read_csv('data-raw/cvpia_trib_names.csv')
-cvpia_watershed <- read_csv('data-raw/cvpia_trib_names.csv') %>%
-  pull(watershed)
-
-# clean mike wright's temperature modeling output
-temperatures <- read_csv('data-raw/tempmaster.csv', skip = 1) %>%
-  mutate(day_month = str_sub(`5q_date`, 1, 6),
-         year = str_sub(`5q_date`, 8, 9),
-         year = str_c('20', year),
-         date = dmy(paste(day_month, year))) %>%
-  select(-day_month, -year, -`5q_date`) %>%
-  gather(watershed, temp_F, -date) %>%
-  group_by(date, watershed) %>%
-  summarise(mean_daily_temp_F = mean(temp_F, na.rm = TRUE),
-            mean_daily_temp_C = (mean_daily_temp_F - 32) * (5/9)) %>%
-  ungroup()
-
-# mike wright has also provided estimates for Antelope Creek, Bear Creek, Elder
-# Creek, Paynes Creek, Bear River, Feather River, and Calaveras River using a
-# regression analysis. More details can be found in
-# 'data-raw/mike_wright_temperature_regression/create_estimated_timeseries.r'
-
-# add additional modeled temperature data from sadie
-monthly_mean_temperature <- temperatures %>%
-  group_by(year = year(date), month = month(date), watershed) %>%
-  summarise(monthly_mean_temp_c = mean(mean_daily_temp_C)) %>%
-  ungroup() %>%
-  mutate(cl_date = ymd(paste(year, month, 1, sep = '-'))) %>%
-  left_join(cl_dates) %>%
-  filter(between(year(cs_date), 1979, 1999)) %>%
-  mutate(date = ymd(paste(year(cs_date), month(cs_date), 1, sep = '-'))) %>%
-  select(date, watershed, monthly_mean_temp_c) %>%
-  bind_rows(read_rds('data-raw/big_chico_creek/big_chico_creek_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/butte_creek/butte_creek_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/cosumnes_river/cosumnes_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/deer_creek/deer_creek_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/lower_sacramento/lower_sac_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/mill_creek/mill_creek_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/mokelumne_river/mokelumne_river_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/yuba_river/yuba_river_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/yolo/yolo_bypass_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/sutter/sutter_bypass_water_temp_c.rds')) %>%
-  bind_rows(read_rds('data-raw/mike_wright_temperature_regression/juv_temp_regression.rds')) %>%
-  spread(watershed, monthly_mean_temp_c) %>%
-  gather(watershed, monthly_mean_temp_c, -date)
-
-juv_temp <- monthly_mean_temperature %>%
-  filter(year(date) > 1979)
-
-devtools::use_data(juv_temp, overwrite = TRUE)
 
 # degree days
 cl_years <- cl_dates %>%
@@ -130,11 +62,4 @@ deg_days %>%
   theme_minimal() +
   facet_wrap(~watershed)
 
-# delta temps ----------------------------------
-dn <- read_rds('data-raw/deltas/north_delta_water_temp_c.rds')
-ds <- read_rds('data-raw/deltas/south_delta_water_temp_c.rds')
-delta_temps <- dn %>%
-  left_join(ds) %>%
-  gather(watershed, monthly_mean_temp_c, -date)
 
-use_data(delta_temps)
